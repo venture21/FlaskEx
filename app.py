b7322fbc1e788fb342386aacab9fa387e313b6b9
venture21 <park0122@gmail.com>
2020-10-30 14:59:55 +0900

    add db

--- app.py
+++ app.py
@@ -1,6 +1,26 @@
+import os
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+# 현재 파일이 실행되는 경로
+basedir = os.path.abspath(os.path.dirname(__file__))
+#db파일이 저장되어 있는 경로
+dbfile = os.path.join(basedir,"db.sqlite")
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+dbfile
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class Test(db.Model):
+    __tablename__ = 'test_table'
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(32), unique=True)
+
+db.create_all()
 
 @app.route('/')
 def hello():
