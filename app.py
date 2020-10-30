96f234cd6f477876b5c2950d5382cc336c161722
venture21 <park0122@gmail.com>
2020-10-30 16:01:25 +0900

    seperate model & controller

--- app.py
+++ app.py
@@ -1,27 +1,27 @@
 import os
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-
-# 현재 파일이 실행되는 경로
-basedir = os.path.abspath(os.path.dirname(__file__))
-#db파일이 저장되어 있는 경로
-dbfile = os.path.join(basedir,"db.sqlite")
+from flask import render_template
+from models import db
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+dbfile
-app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-db = SQLAlchemy(app)
-
-class Test(db.Model):
-    __tablename__ = 'test_table'
-    id = db.Column(db.Integer, primary_key = True)
-    name = db.Column(db.String(32), unique=True)
-
-db.create_all()
-
 @app.route('/')
 def hello():
-    return 'Hello World!'
+    return render_template('hello.html')
+
+if __name__ =="__main__":
+    # 현재 파일이 실행되는 경로
+    basedir = os.path.abspath(os.path.dirname(__file__))
+    #db파일이 저장되어 있는 경로
+    dbfile = os.path.join(basedir,"db.sqlite")
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+dbfile
+    app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.init_app(app)
+    db.app=app
+    db.create_all()
+    app.run(host='127.0.0.1', port=5000, debug=True)
+
+
